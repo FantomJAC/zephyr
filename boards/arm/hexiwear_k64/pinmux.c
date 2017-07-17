719bcefa296f059008042a6752d2dbd99dd9a715
Shotaro Uchida <fantom@xmaker.mx>
2017-07-17 20:38:41 +0900

    hexiwear_k64f: Additional pinmux init

--- boards/arm/hexiwear_k64/pinmux.c
+++ boards/arm/hexiwear_k64/pinmux.c
@@ -39,6 +39,50 @@ static int hexiwear_k64_pinmux_init(struct device *dev)
 	pinmux_pin_set(portc,  9, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(portd,  0, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
+	/* Vibration Motor */
+	pinmux_pin_set(portb,  9, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+#ifdef CONFIG_SPI_0
+	/* SPI0 CS2, CS1, CS0, SCK, SOUT, SIN - ext SPI */
+	pinmux_pin_set(portc,  2, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc,  3, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc,  5, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc,  6, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc,  7, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#ifdef CONFIG_SPI_1
+	/* SPI1 CS0, SCK, SOUT, SIN - Flash/BLE */
+	pinmux_pin_set(portd,  4, PORT_PCR_MUX(kPORT_MuxAlt7));
+	pinmux_pin_set(portd,  5, PORT_PCR_MUX(kPORT_MuxAlt7));
+	pinmux_pin_set(portd,  6, PORT_PCR_MUX(kPORT_MuxAlt7));
+	pinmux_pin_set(portd,  7, PORT_PCR_MUX(kPORT_MuxAlt7));
+#endif
+	/* BLE Misc - PB18, BLE_RST, BLE_WKP */
+	pinmux_pin_set(porte, 26, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portb, 23, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portd, 14, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+#ifdef CONFIG_SPI_2
+	/* SPI2 CS0, SCK, SOUT - ssd1351 */
+	pinmux_pin_set(portb, 20, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 21, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 22, PORT_PCR_MUX(kPORT_MuxAlt2));
+
+	/* PTD15, PTE6 - ssd1351 control */
+	pinmux_pin_set(portd, 15, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(porte,  6, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+	/* BOOST_EN (OLED Power): ONE - Enable, ZERO - Disable */
+	pinmux_pin_set(portc, 13, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+	struct device *gpioc = device_get_binding(CONFIG_GPIO_MCUX_PORTC_NAME);
+
+	gpio_pin_configure(gpioc, 13, GPIO_DIR_OUT);
+	gpio_pin_write(gpioc, 13, 1);
+#endif
+
 #if CONFIG_I2C_0
 	/* I2C0 SCL, SDA - heart rate, light, humidity */
 	pinmux_pin_set(portb,  0, PORT_PCR_MUX(kPORT_MuxAlt2)
@@ -46,14 +90,17 @@ static int hexiwear_k64_pinmux_init(struct device *dev)
 	pinmux_pin_set(portb,  1, PORT_PCR_MUX(kPORT_MuxAlt2)
 					| PORT_PCR_ODE_MASK);
 
-	/* 3V3B_EN */
-	pinmux_pin_set(portb, 12, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	/* 3V3B_EN: ZERO - Enable, Hi-Z - Disable  */
+	pinmux_pin_set(portb, 12, PORT_PCR_MUX(kPORT_MuxAsGpio)
+					| PORT_PCR_ODE_MASK);
 
 	struct device *gpiob = device_get_binding(CONFIG_GPIO_MCUX_PORTB_NAME);
 
 	gpio_pin_configure(gpiob, 12, GPIO_DIR_OUT);
 	gpio_pin_write(gpiob, 12, 0);
 #endif
+	/* TSL2561 INT */
+	pinmux_pin_set(portc,  0, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
 #if CONFIG_I2C_1
 	/* I2C1 SCL, SDA - accel/mag, gyro, pressure */
@@ -62,16 +109,36 @@ static int hexiwear_k64_pinmux_init(struct device *dev)
 	pinmux_pin_set(portc, 11, PORT_PCR_MUX(kPORT_MuxAlt2)
 					| PORT_PCR_ODE_MASK);
 #endif
-	/* FXAS21002 INT2 */
+	/* FXAS21002C RST, INT1, INT2 */
+	pinmux_pin_set(portc, 15, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portd,  1, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(portc, 18, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
-	/* FXOS8700 INT2 */
+	/* FXOS8700 RST, INT1, INT2 */
+	pinmux_pin_set(portd, 11, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portc,  1, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(portd, 13, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+	/* MPL3115A2 INT1, INT2 */
+	pinmux_pin_set(portd, 12, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portd, 10, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
 #ifdef CONFIG_UART_MCUX_0
 	/* UART0 RX, TX */
 	pinmux_pin_set(portb, 16, PORT_PCR_MUX(kPORT_MuxAlt3));
 	pinmux_pin_set(portb, 17, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
+#ifdef CONFIG_UART_MCUX_2
+	/* UART2 RX, TX */
+	pinmux_pin_set(portd,  2, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portd,  3, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
+#ifdef CONFIG_UART_MCUX_3
+	/* UART3 RX, TX */
+	pinmux_pin_set(portc, 16, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portc, 17, PORT_PCR_MUX(kPORT_MuxAlt3));
 #endif
 
 #ifdef CONFIG_UART_MCUX_4
@@ -81,7 +148,10 @@ static int hexiwear_k64_pinmux_init(struct device *dev)
 #endif
 
 #ifdef CONFIG_MAX30101
-	/* LDO - MAX30101 power supply */
+	/* MAX30101 INT */
+	pinmux_pin_set(portb, 18, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+	/* LDO_EN: ONE - Enable, ZERO - Disable  */
 	pinmux_pin_set(porta, 29, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
 	struct device *gpioa = device_get_binding(CONFIG_GPIO_MCUX_PORTA_NAME);
@@ -89,6 +159,15 @@ static int hexiwear_k64_pinmux_init(struct device *dev)
 	gpio_pin_configure(gpioa, 29, GPIO_DIR_OUT);
 	gpio_pin_write(gpioa, 29, 1);
 #endif
+
+	/* TODO: SDHC */
+	/* TODO: I2S */
+
+	/* BAT_SENS_EN: ZERO - Enable, Hi-Z - Disable */
+	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAsGpio)
+					| PORT_PCR_ODE_MASK);
+	/* CHG_STATE */
+	pinmux_pin_set(portc, 12, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
 	return 0;
 }
